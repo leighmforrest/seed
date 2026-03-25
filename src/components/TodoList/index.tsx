0db5785025f322464a99f3ce49903ca98343ed51
Leigh Michael Forrest <leighmforrest@gmail.com>
2026-03-25 19:50:48 -0400

    link click optimization

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
 
 import { type TodoListProps } from "@/types";
 
 import styles from "./styles.module.css";
+import { getTodo } from "@/httpService/todos";
 
 const TodoList = ({ todos }: TodoListProps) => {
+  const queryClient = useQueryClient();
+
   return (
     <ul className={styles.todoList}>
       {todos ? (
         todos.map((todo) => (
           <li key={todo.id}>
-            <Link to={`${todo.id}`} className={styles.todoLink}>{todo.title}</Link>
+            <Link to={`${todo.id}`} 
+            className={styles.todoLink}
+            onMouseEnter={()=> queryClient.prefetchQuery({
+              queryKey: ["todo", todo.id],
+              queryFn: () => getTodo(`${todo.id}`),
+            })}
+            >{todo.title}</Link>
           </li>
         ))
       ) : (
