08d7d727a68ed7db1c32fc9f0f5e121df36aa7e5
Leigh Michael Forrest <leighmforrest@gmail.com>
2026-03-26 18:53:37 -0400

    minimally viable navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,31 @@
+import { Link, NavLink } from "react-router-dom";
+
 import styles from "./styles.module.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState<boolean>(false)
+  const hamburgerClickHandler = () =>{ setOpen(prev => !prev);
+  }
   return (
     <header>
-      <nav className={styles.nav}>Seed Project</nav>
+      <nav className={styles.nav}>
+        <div className={styles.brand}>
+          <Link to="/" className={styles.brandLink}>Seed Project</Link>
+        </div>
+        <div className={`${styles.menu} ${open ? styles.open : ""}`}>
+          <ul>
+            <li><NavLink to="/" className={styles.menuLink}>Home</NavLink></li>
+          </ul>
+        </div>
+        <button onClick={hamburgerClickHandler}
+        className={`${styles.hamburger} ${open && styles.openHamburger}`}
+        >
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+        </button>
+      </nav>
     </header>
   );
 };
